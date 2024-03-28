4c5432c5cddb7898c05a260c5f6cc17746a4728c
Lexi Blesius <Ablesius@users.noreply.github.com>
2024-03-28 15:11:30 +0100

    Spawn camera

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,14 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        // this crashes when closing the window with https://github.com/bevyengine/bevy/issues/11734
-        // .add_systems(Startup, setup_camera)
-        // this does NOT crash, instead has this: https://gist.github.com/Ablesius/f8ed4b0a7252e49d1a4380189cae19eb
-        // try: replaced call to do_nothing with ()
-        .add_systems(Startup, do_nothing)
+        // this crashes when closing the window
+        // but behaves otherwise; see
+        // https://github.com/bevyengine/bevy/issues/11734
+        .add_systems(Startup, setup_camera)
         .add_plugins(DefaultPlugins)
         .run();
 }
 
-fn _setup_camera(mut commands: Commands) {
-    // need to deviate from the blog entry
-    // based on https://github.com/marcusbuffett/bevy_snake/commit/d7f263735dd8cc7ef5d94b961f1f7554ece780a3#r113302872
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
-
-// TODO: drop-in
-// remove as soon as _setup_camera works
-fn do_nothing() {}
