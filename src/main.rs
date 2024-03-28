7212be1081f9787645ae4499998e0fd20ca5f6b2
Lexi Blesius <Ablesius@users.noreply.github.com>
2024-03-28 16:51:10 +0100

    Add snake head
    Had to deviate from the blog post a bit here. Instead of spawn_bundle,
    there is spawn_batch now; only that takes an iterator, not a single
    bundle. It works with `spawn()` though.
    
    I don't quite understand how/why `insert()` is used here yet.

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,34 @@ fn main() {
         // but behaves otherwise; see
         // https://github.com/bevyengine/bevy/issues/11734
         .add_systems(Startup, setup_camera)
+        .add_systems(Startup, spawn_snake)
         .add_plugins(DefaultPlugins)
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+#[derive(Component)]
+struct SnakeHead;
+
+const SNAKE_HEAD_COLOUR: Color = Color::rgb(0.7, 0.7, 0.7);
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn(
+            SpriteBundle {
+                sprite: Sprite {
+                    color: SNAKE_HEAD_COLOUR,
+                    ..default()
+                },
+                transform: Transform {
+                    scale: Vec3::new(10.0, 10.0, 10.0),
+                    ..default()
+                },
+                ..default()
+            }
+        )
+        .insert(SnakeHead);
 }
