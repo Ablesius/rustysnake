4f25f1bc07d91b1f73251e3bcd9376bef5cc7b7b
Lexi Blesius <Ablesius@users.noreply.github.com>
2024-04-02 15:53:15 +0200

    Move snake up forever
    This is just a first draft version, where the snake keeps moving
    upwards. Movement to respond to input will be updated later.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ fn main() {
         // https://github.com/bevyengine/bevy/issues/11734
         .add_systems(Startup, setup_camera)
         .add_systems(Startup, spawn_snake)
+        // needs to be in Update; if we leave it in
+        // Startup, it'll only be executed once. Makes sense!
+        .add_systems(Update, move_snake)
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -34,4 +37,12 @@ fn spawn_snake(mut commands: Commands) {
             ..default()
         })
         .insert(SnakeHead);
+}
+
+fn move_snake(mut head_positions: Query<(&SnakeHead, &mut Transform)>) {
+    for (_head, mut transform) in head_positions.iter_mut() {
+        // for now just move the head 2 upwards
+        // as we don't have user input yet
+        transform.translation.y += 2.0;
+    }
 }
