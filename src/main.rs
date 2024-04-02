25611c32e9eaa7c4040286d79166576bbc14e5bd
Lexi Blesius <Ablesius@users.noreply.github.com>
2024-04-02 21:55:00 +0200

    Remove comment
    The problem noted in the comment no longer exists, probably due to
    updating the rust version.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        // this crashes when closing the window
-        // but behaves otherwise; see
-        // https://github.com/bevyengine/bevy/issues/11734
         .add_systems(Startup, setup_camera)
         .add_systems(Startup, spawn_snake)
         // needs to be in Update; if we leave it in
